a91fdac42502c0cf4bff2bb1c90e934dc00136a6
unknown <giaqui140302@gmail.com>
2024-07-04 16:08:03 +0700

    led_auto_on_off

--- Yolo_UNO/Yolo_UNO.ino
+++ Yolo_UNO/Yolo_UNO.ino
@@ -1,23 +1,47 @@
 #include <Adafruit_NeoPixel.h>
 
-// khởi tạo LED màu chân D3
 Adafruit_NeoPixel rgb(4, D3, NEO_GRB + NEO_KHZ800);
 
-void setup() {
+void setup()
+{
+    Serial.begin(115200); // Khởi tạo Serial
     rgb.begin();
+    // Cấu hình chân GPIO
+    pinMode(D3, OUTPUT); // Chân GPIO điều khiển đèn LED
+    pinMode(A0, INPUT); // Chân analog đọc cảm biến ánh sáng
 }
 
-void loop() {
-    rgb.setPixelColor(0, rgb.Color(255,102,0));
-    rgb.setPixelColor(1, rgb.Color(255,102,0));
-    rgb.setPixelColor(2, rgb.Color(255,102,0));
-    rgb.setPixelColor(3, rgb.Color(255,102,0));
-    rgb.show();
-    delay(1000);
-    rgb.setPixelColor(0, rgb.Color(0,0,0));
-    rgb.setPixelColor(1, rgb.Color(0,0,0));
-    rgb.setPixelColor(2, rgb.Color(0,0,0));
-    rgb.setPixelColor(3, rgb.Color(0,0,0));
-    rgb.show();
-    delay(1000);
+void onLed() {
+  rgb.setPixelColor(0, rgb.Color(255,102,0));
+  rgb.setPixelColor(1, rgb.Color(255,102,0));
+  rgb.setPixelColor(2, rgb.Color(255,102,0));
+  rgb.setPixelColor(3, rgb.Color(255,102,0));
+  rgb.show();
+}
+
+void offLed() {
+  rgb.setPixelColor(0, rgb.Color(0,0,0));
+  rgb.setPixelColor(1, rgb.Color(0,0,0));
+  rgb.setPixelColor(2, rgb.Color(0,0,0));
+  rgb.setPixelColor(3, rgb.Color(0,0,0));
+  rgb.show();
+}
+
+void loop()
+{
+    // Đọc giá trị cảm biến
+    int sensorValue = analogRead(A0);
+
+    // Điều khiển đèn LED
+    if (sensorValue < 1000) { // Bật đèn LED khi ánh sáng thấp
+        onLed();
+    } else {  // Tắt đèn LED khi ánh sáng cao
+        offLed();
+  }
+
+    // In ra giá trị ánh sáng
+    Serial.print("Light: ");
+    Serial.println(sensorValue);
+
+    delay(1000); // Chờ 1 giây
 }
